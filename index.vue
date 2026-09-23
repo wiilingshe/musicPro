a8b7465cdd0b5835771a314230a1235e712ad8c1
wiilingshe <1214220846@qq.com>
2026-09-23 09:45:19 +0800

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="componentDetails">
+  <div class="componentDetails">11
     <div class="componentDetails-left" id="componentDetails-left">
       <el-input :placeholder="$t('m.componentDetails16')" v-model="filterText"></el-input>
 
